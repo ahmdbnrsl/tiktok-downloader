ac756a8697a90d7e730aaf981b2aca55596700d0
abrazax56 <ahmadbenirusli@gmail.com>
2024-02-01 09:06:03 +0700

    eight-commit

--- src/components/Elements/Back.jsx
+++ src/components/Elements/Back.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <Link to="/" className="px-8 max-w-4xl text-cyan-300 text-sm font-mono">&laquo; Back to home</Link>
+    <div className="w-full flex justify-center">
+      <Link to="/" className="px-8 max-w-4xl text-cyan-300 text-sm font-mono">&laquo; Back to home</Link>
+    </div>
   )
 }
