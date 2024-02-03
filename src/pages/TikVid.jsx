c3f5a1a1cb8ccb097ff4372f9fd4c8f698f3f4f6
abrazax56 <ahmadbenirusli@gmail.com>
2024-02-04 06:23:03 +0700

    ten-commit

--- src/pages/TikVid.jsx
+++ src/pages/TikVid.jsx
@@ -4,36 +4,39 @@ import Footer from '.././components/Layouts/Footer.jsx';
 import Form from '.././components/Layouts/Form.jsx';
 import Button from '.././components/Elements/Submit.jsx';
 import Loading from '.././components/Elements/Loading.jsx';
-import Back from '.././components/Elements/Back.jsx'
+import Back from '.././components/Elements/Back.jsx';
 
 export default function () {
   const getValue = () => document.querySelector(".input-urls").value;
   const fetchData = async(fetchUrl) => {
     return await fetch(fetchUrl).then(_ => _.json()).then(_ => {
-      const urls = _[0].video;
       document.querySelector(".submiter").classList.toggle("hidden");
       document.querySelector(".loadny").classList.toggle("hidden");
-      return urls
+      if(_.result.type === "video") {
+        return _.result.video1
+      } else {
+        return _.result.images
+      }
     })
       .catch(_ => {
         document.querySelector(".submiter").classList.toggle("hidden");
         document.querySelector(".loadny").classList.toggle("hidden");
-        window.navigator.vibrate(500)
+        window.navigator.vibrate(500);
         setTimeout(() => window.location.reload(false), 500);
       });
   };
 
   const HandleSubmit = async(e) => {
     e.preventDefault();
-    let tautan = getValue()
-    const fetchUrl = `https://downtik.netlify.app/download?url=${tautan}`;
+    let tautan = getValue();
+    const fetchUrl = `https://downtik.netlify.app/dl?url=${tautan}`;
     document.querySelector(".submiter").classList.toggle("hidden");
     document.querySelector(".loadny").classList.toggle("hidden");
-    const taut = await fetchData(fetchUrl)
+    const taut = await fetchData(fetchUrl);
     
     const a = document.createElement('a');
     if(typeof(taut) === "object") {
-      a.href = "/image"
+      a.href = "/image";
       document.querySelector(".input-urls").value = "media is image! please use tiktok slide photo downloader";
       window.navigator.vibrate(100);
       setTimeout(() => {
